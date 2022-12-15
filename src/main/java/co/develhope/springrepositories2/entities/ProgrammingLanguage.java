183c7e617c73d2f501812118858d33603c351c79
Giovanni <giosal21@gmail.com>
2022-12-15 11:36:38 +0100

    eseSpringRepo

---
+++ src/main/java/co/develhope/springrepositories2/entities/ProgrammingLanguage.java
@@ -0,0 +1,24 @@
+package co.develhope.springrepositories2.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Table
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProgrammingLanguage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(nullable = false)
+    private String name;
+    private Integer firstAppearence;
+    @Column(nullable = false)
+    private String inventor;
+
+}
